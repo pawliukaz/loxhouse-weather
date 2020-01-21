0332f6278593da122adfad19cae045bcedcd70cd
Paulius Stasiulionis <paulius@dappradar.com>
2020-01-21 14:47:27 +0200

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,12 @@
 mkdir /var/www/releases/release
 git clone git@github.com:pawliukaz/loxhouse-weather.git /var/www/releases/release
 cd /var/www/releases/release && ln -s /var/www/shared/.env .env
+cd /var/www/releases/release && mkdir var
 cd /var/www/releases/release && ln -s /var/www/shared/var/log var/log
 cd /var/www/releases/release && composer install
-cd /var/www/releases/release && yarn
+cd /var/www/releases/release && yarn && yarn encore production
 cd /var/www/releases/release && yarn encore production
+$dateValue = $(date +%Y%m%d%H%M%S)
+cd /var/www/releases && mkdir $dateValue
+cd /var/www/releases && mv releases $dateValue
+ln -l /var/www/releases/$dateValue /var/www/current
