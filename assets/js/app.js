5cb8aea9dba2baa6d2cdd83ba3451d8b6fdaca49
Paulius Stasiulionis <paulius@dappradar.com>
2020-01-21 14:27:41 +0200

    Weather and deploy

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 const $ = require('jquery');
 require('bootstrap');
 require('datatables.net');
-import 'datatables.net-dt/css/jquery.datatables.css';
+import 'datatables.net-dt/css/jquery.dataTables.css';
 
 $(document).ready( function () {
     $('#weatherTable').DataTable();
--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@
 mkdir /var/www/releases/release
 git clone git@github.com:pawliukaz/loxhouse-weather.git /var/www/releases/release
 cd /var/www/releases/release && ln -s /var/www/shared/.env .env
+cd /var/www/releases/release && ln -s /var/www/shared/var/log var/log
 cd /var/www/releases/release && composer install
-cd /var/www/releases/release && yarn install prodution
+cd /var/www/releases/release && yarn
+cd /var/www/releases/release && yarn encore production
