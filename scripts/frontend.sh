4e8ab9ca8ad5aa57da0d41778524f886dd9dbd3e
Paulius Stasiulionis <ps@aemetriks.com>
2026-07-08 13:58:50 +0300

    Update docker & minor updates on composer pagckages json

--- scripts/frontend.sh
+++ scripts/frontend.sh
@@ -10,16 +10,16 @@ popd > /dev/null
 
 # Prerequisite
 docker --version > /dev/null 2>&1 || { echo >&2 "Docker not found. Please install it via https://docs.docker.com/install/"; exit 1; }
-docker-compose --version > /dev/null 2>&1 || { echo >&2 "Docker-compose not found. Please install it via https://docs.docker.com/compose/install/"; exit 1; }
+docker compose --version > /dev/null 2>&1 || { echo >&2 "Docker-compose not found. Please install it via https://docs.docker.com/compose/install/"; exit 1; }
 
 # Entering into Node.js container (simulating SSH/terminal)
 if [ "$ARGS" != "" ]; then
     echo "Executing in Node.js (frontend) container: $ARGS"
-    docker-compose -f "$SCRIPT_DIR/docker-compose-tools.yml" run frontend.symfony $ARGS
+    docker compose -f "$SCRIPT_DIR/docker-compose-tools.yml" run frontend.symfony $ARGS
 else
     echo "JavaScript/CSS dependencies can be installed via: yarn"
     echo "Assets compiling for production: yarn run encore production"
     echo "Assets compiling during development: yarn run encore dev --watch"
     echo 'Type "exit" to get out of terminal'
-    docker-compose -f "$SCRIPT_DIR/docker-compose-tools.yml" run frontend.symfony
+    docker compose -f "$SCRIPT_DIR/docker-compose-tools.yml" run frontend.symfony
 fi
